397c49907cc0ee629286372b9778f53f0981c9dc
Dominik Goltermann <dominik.goltermann@runtastic.com>
2017-04-23 16:44:16 +0200

    added type coercion for Types::Type definitions via lambda

--- lib/definition/types/type.rb
+++ lib/definition/types/type.rb
@@ -5,22 +5,41 @@ require "definition/errors/invalid"
 module Definition
   module Types
     class Type < Base
-      def initialize(name, klass)
+      def initialize(name, klass, coerce: false)
         self.klass = klass
+        self.coerce = coerce
         super(name)
       end
 
       def conform(value)
-        if value.is_a?(klass)
+        unless valid?(value)
+          coerced_value = coerced(value)
+          return try_conform(coerced_value)
+        end
+
+        try_conform(value)
+      end
+
+      private
+
+      def valid?(value)
+        value.is_a?(klass)
+      end
+
+      def try_conform(value)
+        if valid?(value)
           [:ok, value]
         else
           [:error, [Errors::Invalid.new(value, name: name, definition: self, description: "is_a? #{klass}")]]
         end
       end
 
-      private
+      def coerced(value)
+        return value unless coerce
+        coerce.call(value)
+      end
 
-      attr_accessor :test, :klass
+      attr_accessor :test, :klass, :coerce
     end
   end
 end
--- spec/lib/definition/types/type_spec.rb
+++ spec/lib/definition/types/type_spec.rb
@@ -4,34 +4,65 @@ require "spec_helper"
 require "definition/types/type"
 
 describe Definition::Types::Type do
-  subject(:conform) { definition.conform(value) }
+  describe ".conform" do
+    subject(:conform) { definition.conform(value) }
 
-  let(:definition) do
-    described_class.new(:type_test,
-                        ::Integer)
-  end
+    let(:definition) do
+      described_class.new(:type_test,
+                          ::Integer,
+                          coerce: coerce)
+    end
+    let(:coerce) { false }
 
-  context "with int value" do
-    let(:value) { 2 }
-    it { is_expected.to conform_with(value) }
-  end
-
-  context "with string value" do
-    let(:value) { "2" }
-    let(:expected_errors) do
-      [
-          {
-            value: value,
-            name: :type_test,
-            description: "is_a? Integer",
-            definition: definition,
-            children: []
-          }
-      ]
+    context "with int value" do
+      let(:value) { 2 }
+      it { is_expected.to conform_with(value) }
     end
 
-    it "generates correct errors" do
-      expect(conform).to not_conform_with(expected_errors)
+    context "with string value" do
+      let(:value) { "2" }
+      let(:expected_errors) do
+        [
+            {
+              value: value,
+              name: :type_test,
+              description: "is_a? Integer",
+              definition: definition,
+              children: []
+            }
+        ]
+      end
+
+      it "generates correct errors" do
+        expect(conform).to not_conform_with(expected_errors)
+      end
+    end
+
+    context "with coercion lambda" do
+      let(:coerce) do
+        lambda { |v| Integer(v) rescue v }
+      end
+
+      context "with coercable value" do
+        let(:value) { "2" }
+        it { is_expected.to conform_with(2) }
+      end
+
+      context "with uncoercable value" do
+        let(:value) { "a2" }
+        let(:expected_errors) do
+          [
+              {
+                value: value,
+                name: :type_test,
+                description: "is_a? Integer",
+                definition: definition,
+                children: []
+              }
+          ]
+        end
+        it { is_expected.to not_conform_with(expected_errors) }
+      end
     end
   end
 end
