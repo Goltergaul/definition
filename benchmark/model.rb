faf306648b0391f25912ae62d1ca85152ade4459
Dominik Goltermann <dominik.goltermann@runtastic.com>
2022-09-15 13:39:07 +0200

    Improved performance of Keys Definition to make the Definition::Model as fast as Dry::Struct

---
+++ benchmark/model.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require "bundler/inline"
+
+gemfile do
+  source "https://rubygems.org"
+  gem "dry-struct", "~> 1.4"
+  gem "awesome_print"
+  gem "benchmark-ips"
+  gem "pry"
+  gem "ruby-prof"
+  gem "definition", path: File.expand_path("../.", __dir__)
+end
+
+class DryStructModel < Dry::Struct
+  schema schema.strict
+
+  attribute :id, Dry::Types["strict.integer"]
+  attribute :app_key, Dry::Types["strict.string"]
+  attribute :app_version, Dry::Types["strict.string"]
+  attribute :app_name, Dry::Types["strict.string"].optional.default(nil)
+  attribute :app_branch, Dry::Types["strict.string"].optional.default(nil)
+  attribute :platform, Dry::Types["strict.string"].optional.default(nil)
+end
+
+class DefinitionModel < Definition::Model
+  required :id, Definition.Type(Integer)
+  required :app_key, Definition.Type(String)
+  required :app_version, Definition.Type(String)
+  optional :app_name, Definition.Type(String)
+  optional :app_branch, Definition.Type(String)
+  optional :platform, Definition.Type(String)
+end
+
+
+puts "Benchmark with valid input data:"
+valid_data = { id: 1, app_key: "com.test", app_version: "1.0.0", app_name: "testapp" }
+
+#RubyProf.start
+#10000.times { DefinitionModel.new(**valid_data) }
+#result = RubyProf.stop
+#
+#printer = RubyProf::CallTreePrinter.new(result)
+#printer.print
+#exit
+
+Benchmark.ips do |x|
+  x.config(time: 5, warmup: 2)
+
+  x.report("definition") do
+    DefinitionModel.new(**valid_data)
+  end
+
+  x.report("dry-struct") do
+    DryStructModel.new(**valid_data)
+  end
+
+  x.compare!
+end
+
+puts "Benchmark with invalid input data:"
+valid_data = { id: "abc", app_key: "com.test", app_name: "testapp" }
+Benchmark.ips do |x|
+  x.config(time: 5, warmup: 2)
+
+  x.report("definition") do
+    DefinitionModel.new(**valid_data) rescue Definition::InvalidModelError
+  end
+
+  x.report("dry-struct") do
+    DryStructModel.new(**valid_data) rescue Dry::Struct::Error
+  end
+
+  x.compare!
+end
--- lib/definition/types/keys.rb
+++ lib/definition/types/keys.rb
@@ -9,11 +9,11 @@ module Definition
     class Keys < Base
       module Dsl
         def required(key, definition)
-          required_definitions[key] = definition
+          required_definitions << { key: key, definition: definition }
         end
 
         def optional(key, definition, **opts)
-          optional_definitions[key] = definition
+          optional_definitions << { key: key, definition: definition }
           default(key, opts[:default]) if opts.key?(:default)
         end
 
@@ -30,11 +30,11 @@ module Definition
           raise ArgumentError.new("Included Definition can only be a Keys Definition") unless other.is_a?(Types::Keys)
 
           ensure_keys_do_not_interfere(other)
-          other.required_definitions.each do |key, definition|
-            required(key, definition)
+          other.required_definitions.each do |hash|
+            required(hash[:key], hash[:definition])
           end
-          other.optional_definitions.each do |key, definition|
-            optional(key, definition)
+          other.optional_definitions.each do |hash|
+            optional(hash[:key], hash[:definition])
           end
           other.defaults.each do |key, default|
             default(key, default)
@@ -62,8 +62,8 @@ module Definition
 
       def initialize(name, req: {}, opt: {}, defaults: {}, options: {})
         super(name)
-        self.required_definitions = req
-        self.optional_definitions = opt
+        self.required_definitions = req.map { |key, definition| { key: key, definition: definition } }
+        self.optional_definitions = opt.map { |key, definition| { key: key, definition: definition } }
         self.defaults = defaults
         self.ignore_extra_keys = options.fetch(:ignore_extra_keys, false)
       end
@@ -73,7 +73,7 @@ module Definition
       end
 
       def keys
-        required_definitions.keys + optional_definitions.keys
+        (required_definitions + optional_definitions).map { |hash| hash[:key] }
       end
 
       class Conformer
@@ -81,13 +81,14 @@ module Definition
           self.definition = definition
           self.value = value
           self.errors = []
+          @conform_result_value = {}
         end
 
         def conform
           if valid_input_type?
-            add_extra_key_errors unless definition.ignore_extra_keys
-            add_missing_key_errors
+            @not_conformed_value_keys = value.dup
             values = conform_all_keys
+            add_extra_key_errors unless definition.ignore_extra_keys
           else
             errors.push(ConformError.new(definition,
                                          "#{definition.name} is not a Hash",
@@ -106,7 +107,7 @@ module Definition
         end
 
         def add_extra_key_errors
-          extra_keys = value.keys - all_keys
+          extra_keys = @not_conformed_value_keys.keys
           return if extra_keys.empty?
 
           extra_keys.each do |key|
@@ -120,58 +121,31 @@ module Definition
         end
 
         def conform_all_keys
-          required_keys_values = conform_definitions(required_definitions)
-          optional_keys_values = conform_definitions(optional_definitions)
+          conform_definitions(definition.required_definitions, required: true)
+          conform_definitions(definition.optional_definitions, required: false)
 
-          definition.defaults.merge(required_keys_values.merge!(optional_keys_values))
+          @conform_result_value
         end
 
-        def all_keys
-          required_keys + optional_keys
-        end
-
-        def required_definitions
-          definition.required_definitions
-        end
-
-        def required_keys
-          required_definitions.keys
-        end
-
-        def optional_definitions
-          definition.optional_definitions
-        end
-
-        def optional_keys
-          optional_definitions.keys
-        end
-
-        def conform_definitions(keys)
-          keys.each_with_object({}) do |(key, key_definition), result_value|
-            next unless value.key?(key)
+        def conform_definitions(keys, required:)
+          keys.each do |hash|
+            key = hash[:key]
+            key_definition = hash[:definition]
+            @not_conformed_value_keys.delete(key)
+            unless value.key?(key)
+              errors.push(missing_key_error(key)) if required
+              @conform_result_value[key] = definition.defaults[key] if definition.defaults.key?(key)
+              next
+            end
 
             result = key_definition.conform(value[key])
-            result_value[key] = result.value
+            @conform_result_value[key] = result.value
             next if result.passed?
 
             errors.push(KeyConformError.new(key_definition,
                                             "#{definition.name} fails validation for key #{key}",
                                             key:        key,
                                             sub_errors: result.error_tree))
-          end
-        end
-
-        def add_missing_key_errors
-          required_definition = Types::Include.new(
-            definition.name,
-            *required_keys
-          )
-
-          result = required_definition.conform(value)
-          return if result.passed?
-
-          result.errors.each do |error|
-            errors.push(missing_key_error(error.key))
           end
         end
 
