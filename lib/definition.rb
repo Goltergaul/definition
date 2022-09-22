d7cc7fb98dca18a675317f750783527737252d46
Dominik Goltermann <dominik.goltermann@runtastic.com>
2022-09-22 15:09:21 +0200

    Add initializer mixin

--- lib/definition.rb
+++ lib/definition.rb
@@ -5,6 +5,7 @@ require "definition/dsl"
 require "definition/dsl/comparators"
 require "definition/dsl/nil"
 require "definition/value_object"
+require "definition/initializer"
 require "definition/model"
 require "definition/i18n"
 
